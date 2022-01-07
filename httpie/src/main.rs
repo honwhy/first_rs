a5066d55afa36f564676d0c628ee2acf6dfc5159
honwhy.wang <honwhy.wang@hotmail.com>
2022-01-08 03:26:31 +0800

    httpie

--- httpie/src/main.rs
+++ httpie/src/main.rs
@@ -1,55 +1,52 @@
-use anyhow::{anyhow, Result};
-use clap::{AppSettings, Clap};
-use colored::*;
-use mime::Mime;
-use reqwest::{header, Client, Response, Url};
 use std::{collections::HashMap, str::FromStr};
 
-use syntect::easy::HighlightLines;
-use syntect::highlighting::{Style, ThemeSet};
-use syntect::parsing::SyntaxSet;
-use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
-//
-//
-///
-#[derive(Clap, Debug)]
-#[clap(version = "1.0", author = "Tyr Chen <tyr@chen.com>")]
-#[clap(setting = AppSettings::ColoredHelp)]
+use clap::{Parser};
+use anyhow::{Result, anyhow};
+use mime::Mime;
+use reqwest::{header, Client, Response, Url};
+use colored::*;
+// 定义HTTPie的CLI的主入口，它包含若干个子命令
+// 下面 /// 的注释是文档，clap会将其作为CLI的帮助
+
+/// A naive httpie implementation with Rust, can you imagine how easy it is
+#[derive(Parser, Debug)]
+#[clap(about, version, author)]
 struct Opts {
     #[clap(subcommand)]
     subcmd: SubCommand,
 }
 
-//
-#[derive(Clap, Debug)]
+// 子命令分别对应不同的HTTP方法，目前只支持 GET / POST
+#[derive(Parser, Debug)]
 enum SubCommand {
     Get(Get),
     Post(Post),
 }
 
-//
-#[derive(Clap, Debug)]
+/// feed get with an url and we will retrieve the response for you
+#[derive(Parser,Debug)]
 struct Get {
     #[clap(parse(try_from_str = parse_url))]
     url: String,
 }
-fn parse_url(s: &str) -> Result<String> {
-    let _url: Url = s.parse()?;
-    Ok(s.into())
-}
-#[derive(Clap, Debug)]
+
+/// feed post with an url and optional key=value pairs. We will post the data
+/// as JSON, and retrieve the response for you
+#[derive(Parser,Debug)]
 struct Post {
     #[clap(parse(try_from_str = parse_url))]
     url: String,
-    #[clap(parse(try_from_str=parse_kv_pair))]
+    #[clap(parse(try_from_str = parse_kv_pair))]
     body: Vec<KvPair>,
 }
+
 #[derive(Debug, PartialEq)]
 struct KvPair {
     k: String,
     v: String,
 }
 impl FromStr for KvPair {
+    
     type Err = anyhow::Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
@@ -61,14 +58,37 @@ impl FromStr for KvPair {
         })
     }
 }
+fn parse_url(s: &str) -> Result<String> {
+    let _url: Url = s.parse()?;
 
+    Ok(s.into())
+}
 fn parse_kv_pair(s: &str) -> Result<KvPair> {
     Ok(s.parse()?)
 }
+#[tokio::main]
+async fn main() -> Result<()>{
+    let opts: Opts = Opts::parse();
+    let mut headers = header::HeaderMap::new();
+    headers.insert("X-POWERED-BY", "Rust".parse()? );
+    headers.insert(header::USER_AGENT, "Rust httpie".parse()?);
+    let client = reqwest::Client::builder()
+        .default_headers(headers)
+        .build()?;
+    let result = match opts.subcmd {
+        SubCommand::Get(ref args) => get(client, args).await?,
+        SubCommand::Post(ref args) => post(client, args).await?,
+    };
+    Ok(result)
+}
+
+
+
 async fn get(client: Client, args: &Get) -> Result<()> {
     let resp = client.get(&args.url).send().await?;
     Ok(print_resp(resp).await?)
 }
+
 async fn post(client: Client, args: &Post) -> Result<()> {
     let mut body = HashMap::new();
     for pair in args.body.iter() {
@@ -77,7 +97,6 @@ async fn post(client: Client, args: &Post) -> Result<()> {
     let resp = client.post(&args.url).json(&body).send().await?;
     Ok(print_resp(resp).await?)
 }
-
 fn print_status(resp: &Response) {
     let status = format!("{:?} {}", resp.version(), resp.status()).blue();
     println!("{}\n", status);
@@ -86,30 +105,42 @@ fn print_headers(resp: &Response) {
     for (name, value) in resp.headers() {
         println!("{}: {:?}", name.to_string().green(), value);
     }
-    print!("\n");
+    println!();
 }
-
 fn print_body(m: Option<Mime>, body: &String) {
-    // Load these once at the start of your program
-    let ps = SyntaxSet::load_defaults_newlines();
-    let ts = ThemeSet::load_defaults();
-    let syntax = ps.find_syntax_by_extension("json").unwrap();
-    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
-
     match m {
         Some(v) if v == mime::APPLICATION_JSON => {
-            //println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
-            let s = jsonxf::pretty_print(body).unwrap();
-            for line in LinesWithEndings::from(&s) {
-                let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
-                let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
-                println!("{}", escaped);
-            }
+           // println!("{}", jsonxf::pretty_print(body).unwrap().cyan())
+           print_syntect(body);
         }
         _ => println!("{}", body),
     }
 }
+use syntect::easy::HighlightLines;
+use syntect::parsing::SyntaxSet;
+use syntect::highlighting::{ThemeSet, Style};
+use syntect::util::{as_24_bit_terminal_escaped, LinesWithEndings};
 
+
+fn print_syntect(body: &String) {
+    // Load these once at the start of your program
+    let ps = SyntaxSet::load_defaults_newlines();
+    let ts = ThemeSet::load_defaults();
+
+    let syntax = ps.find_syntax_by_extension("json").unwrap();
+    let mut h = HighlightLines::new(syntax, &ts.themes["base16-ocean.dark"]);
+    //let s = "pub struct Wow { hi: u64 }\nfn blah() -> u64 {}";
+    for line in LinesWithEndings::from(body.as_str()) {
+        let ranges: Vec<(Style, &str)> = h.highlight(line, &ps);
+        let escaped = as_24_bit_terminal_escaped(&ranges[..], true);
+        print!("{}", escaped);
+    }
+}
+fn get_content_type(resp: &Response) -> Option<Mime> {
+    resp.headers()
+        .get(header::CONTENT_TYPE)
+        .map(|v| v.to_str().unwrap().parse().unwrap())
+}
 async fn print_resp(resp: Response) -> Result<()> {
     print_status(&resp);
     print_headers(&resp);
@@ -119,27 +150,6 @@ async fn print_resp(resp: Response) -> Result<()> {
     Ok(())
 }
 
-fn get_content_type(resp: &Response) -> Option<Mime> {
-    resp.headers()
-        .get(header::CONTENT_TYPE)
-        .map(|v| v.to_str().unwrap().parse().unwrap())
-}
-#[tokio::main]
-async fn main() -> Result<()> {
-    let opts: Opts = Opts::parse();
-    let mut headers = header::HeaderMap::new();
-    headers.insert("X-POWERED-BY", "Rust".parse()?);
-    headers.insert(header::USER_AGENT, "Rust Httpie".parse()?);
-    let client = reqwest::Client::builder()
-        .default_headers(headers)
-        .build()?;
-    let result = match opts.subcmd {
-        SubCommand::Get(ref args) => get(client, args).await?,
-        SubCommand::Post(ref args) => post(client, args).await?,
-    };
-
-    Ok(result)
-}
 
 #[cfg(test)]
 mod tests {
@@ -155,16 +165,13 @@ mod tests {
     #[test]
     fn parse_kv_pair_works() {
         assert!(parse_kv_pair("a").is_err());
-        assert_eq!(
-            parse_kv_pair("a=1").unwrap(),
+        assert_eq!(parse_kv_pair("a=1").unwrap(),
             KvPair {
                 k: "a".into(),
                 v: "1".into(),
             }
         );
-
-        assert_eq!(
-            parse_kv_pair("b=").unwrap(),
+        assert_eq!(parse_kv_pair("b=").unwrap(),
             KvPair {
                 k: "b".into(),
                 v: "".into(),
